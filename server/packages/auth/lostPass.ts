6f1cd07d3a0304ba2d27fe8f02b3884862a519d4
filipeclegnani <filipeclegnani@gmail.com>
2022-11-03 22:06:54 -0300

    redefinição de senha criada e funcional

--- server/packages/auth/lostPass.ts
+++ server/packages/auth/lostPass.ts
@@ -1,61 +1,59 @@
-import nodemailer from 'nodemailer'
-import { Request, Response } from 'express'
-import Config from '../../config'
-import { uuid } from 'uuidv4'
-import moment from 'moment'
+import nodemailer from "nodemailer";
+import { Request, Response } from "express";
+import Config from "../../config";
+import { uuid } from "uuidv4";
+import moment from "moment";
 
 export default async function lostPass(request: Request, response: Response) {
-    const config = await Config.getInstance()
-    const banco = config.banco
-    const { email } = request.body
+  const config = await Config.getInstance();
+  const banco = config.banco;
+  const { email } = request.body;
 
-    if (!banco) {
-        return response.status(500).json({
-            key: 'internalServerError',
-            msg: 'Erro interno do servidor',
-        })
-    }
-    const mail = config.configuration.mailInformation
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: mail.email,
-            pass: mail.senha
-        },
+  if (!banco) {
+    return response.status(500).json({
+      key: "internalServerError",
+      msg: "Erro interno do servidor",
     });
-    const existentUser = await banco
-        .collection('usuarios')
-        .findOne({ email: email })
+  }
+  const mail = config.configuration.mailInformation;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: mail.email,
+      pass: mail.senha,
+    },
+  });
+  const existentUser = await banco
+    .collection("usuarios")
+    .findOne({ email: email });
 
-
-    if (!existentUser) {
-        return response.status(422).json({
-            key: 'inexistentUser',
-            msg: 'Usuário inexistente',
-        })
-    }
-    const now = moment().toDate()
-
-    const chave = uuid()
-
-    await banco.collection('forgot_password').insertMany([
-        { email, chave, cretae_at: now }
-    ])
-
-    const mailOptions = {
-        from: 'contatojuliaprestes@gmail.com',
-        to: 'prestesjulea@gmail.com',
-        subject: 'Redefinição de senha',
-        html: `<a href="localhost:3000/redefinirSenha/${chave}">Redefinir senha<a>`
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.error(error);
-        }
-    });
+  if (!existentUser) {
     return response.status(422).json({
-        key: 'sucessfulyPasswordResetRequest',
-        msg: 'Sucesso ao requisitar troca de senha',
-    })
-}
+      key: "inexistentUser",
+      msg: "Usuário inexistente",
+    });
+  }
+  const now = moment().toDate();
 
+  const chave = uuid();
+
+  await banco
+    .collection("forgot_password")
+    .insertMany([{ email, chave, created_at: now, used: false }]);
+
+  const mailOptions = {
+    from: "contatojuliaprestes@gmail.com",
+    to: "prestesjulea@gmail.com",
+    subject: "Redefinição de senha",
+    html: `<a href="localhost:3000/redefinirSenha/${chave}">Redefinir senha<a>`,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.error(error);
+    }
+  });
+  return response.status(422).json({
+    key: "sucessfulyPasswordResetRequest",
+    msg: "Sucesso ao requisitar troca de senha",
+  });
+}
--- server/packages/auth/redefinePass.ts
+++ server/packages/auth/redefinePass.ts
@@ -1,42 +1,51 @@
-import { Request, Response } from 'express'
-import moment from 'moment'
-import Config from '../../config'
-import bcrypt from 'bcrypt'
+import { Request, Response } from "express";
+import moment from "moment";
+import Config from "../../config";
+import bcrypt from "bcrypt";
 
 export default async function lostPass(request: Request, response: Response) {
-    const config = await Config.getInstance()
-    const banco = config.banco
-    const { chave, senha } = request.body
+  const config = await Config.getInstance();
+  const banco = config.banco;
+  const { chave, senha } = request.body;
 
-    if (!banco) {
-        return response.status(500).json({
-            key: 'internalServerError',
-            msg: 'Erro interno do servidor',
-        })
-    }
+  if (!banco) {
+    return response.status(500).json({
+      key: "internalServerError",
+      msg: "Erro interno do servidor",
+    });
+  }
+  const now = moment();
 
-    const now = moment()
-    const passRecovery = await banco
-        .collection('forgot_password')
-        .findOne({ chave, created_at: { $lte: now.subtract({ hour: 1 }).toDate() } })
+  const passRecovery = await banco.collection("forgot_password").findOne({
+    chave,
+    created_at: { $gt: now.subtract({ hour: 1 }).toDate() },
+    used: false,
+  });
 
-
-    if (!passRecovery) {
-        return response.status(422).json({
-            key: 'inexistentUser',
-            msg: 'Usuário inexistente',
-        })
-    }
-
-    const salt = await bcrypt.genSalt(12)
-    const hash = await bcrypt.hash(senha, salt)
-
-    await banco.collection('usuarios').updateOne({ email: passRecovery.email }, { hash })
-
-
+  if (!passRecovery) {
     return response.status(422).json({
-        key: 'sucessfulyPasswordReset',
-        msg: 'Sucesso ao trocar de senha',
-    })
-}
+      key: "inexistentUser",
+      msg: "Usuário inexistente",
+    });
+  }
 
+  await banco.collection("forgot_password").updateOne(
+    {
+      chave,
+      created_at: { $gt: now.subtract({ hour: 1 }).toDate() },
+      used: false,
+    },
+    { $set: { used: true } }
+  );
+  const salt = await bcrypt.genSalt(12);
+  const hash = await bcrypt.hash(senha, salt);
+
+  await banco
+    .collection("usuarios")
+    .updateOne({ email: passRecovery.email }, { $set: { hash } });
+
+  return response.status(422).json({
+    key: "sucessfulyPasswordReset",
+    msg: "Sucesso ao trocar de senha",
+  });
+}
