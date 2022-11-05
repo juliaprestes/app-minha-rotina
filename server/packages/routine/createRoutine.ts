c70805fca5601e8fd591219e4b029304f07f2380
filipeclegnani <filipeclegnani@gmail.com>
2022-11-04 21:16:53 -0300

    jwt funcionando

--- server/packages/routine/createRoutine.ts
+++ server/packages/routine/createRoutine.ts
@@ -5,7 +5,19 @@ import jwt from "jsonwebtoken";
 export default async function lostPass(request: Request, response: Response) {
   const config = await Config.getInstance();
   const banco = config.banco;
-  const { rotina, token } = request.body;
+  const { rotina, token1 } = request.body;
+
+  const token = jwt.sign(
+    {
+      id: 'existentUser._id',
+      nome: 'existentUser.nome',
+      email: 'existentUser.email',
+      nivelAcesso: 'existentUser.nivelAcesso',
+    },
+    'disuhadhpsdoasdyuiosd'
+  )
+  console.log(token)
+  const token2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImV4aXN0ZW50VXNlci5faWQiLCJub21lIjoiZXhpc3RlbnRVc2VyLm5vbWUiLCJlbWFpbCI6ImV4aXN0ZW50VXNlci5lbWFpbCIsIm5pdmVsQWNlc3NvIjoiZXhpc3RlbnRVc2VyLm5pdmVsQWNlc3NvIiwiaWF0IjoxNjY3NjA2NTQwfQ.JG00DP7DSxQRXzu5y5UQfY8z8f5X6iXZmhlvB1OAUFM'
 
   // if (!banco) {
   //   return response.status(500).json({
@@ -13,14 +25,19 @@ export default async function lostPass(request: Request, response: Response) {
   //     msg: "Erro interno do servidor",
   //   });
   // }
-  const verifyedToken = await jwt.verify(
-    token,
-    config.configuration.security.secret,
-    {}
-  );
+  try {
+    const verifyedToken =  jwt.verify(
+      token2,
+      config.configuration.security.secret,
+      {}
+    );
   console.log("verifyedToken", verifyedToken);
-  const decodeToken = await jwt.decode(token, {});
-  console.log("decodeToken", decodeToken);
+  } catch (error) {
+    return response.status(422).json({
+      key:'tokenFailure',
+      msg:'erro inesperado no token'
+    })
+  }
 
   return response.status(422).json({
     key: "sucessfulyRoutineCreated",
