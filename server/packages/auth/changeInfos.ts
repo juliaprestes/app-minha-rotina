fa5788950beb73160e6f8c07deacb5f80a9d9213
filipeclegnani <filipeclegnani@gmail.com>
2022-11-28 08:33:20 -0300

    back do change infos feito

--- server/packages/auth/changeInfos.ts
+++ server/packages/auth/changeInfos.ts
@@ -1,52 +1,35 @@
 import { Request, Response } from "express";
 import moment from "moment";
 import Config from "../../config";
-import bcrypt from "bcrypt";
+import validateToken from "../../utils/validateToken";
 
-export default async function changeInfos(request: Request, response: Response) {
-    const config = await Config.getInstance();
-    const banco = config.banco;
-    const { token, email, nome } = request.body;
+export default async function changeInfos(
+  request: Request,
+  response: Response
+) {
+  const config = await Config.getInstance();
+  const banco = config.banco;
+  const { token, email, nome } = request.body;
 
-    if (!banco) {
-        return response.status(500).json({
-            key: "internalServerError",
-            msg: "Erro interno do servidor",
-        });
-    }
-    const now = moment();
-
-    const existentUser = await banco
-        .collection("usuarios")
-        .findOne({
-            token,
-            created_at: { $gt: now.subtract({ hour: 1 }).toDate() },
-            used: false,
-        })
-
-
-    if (!existentUser) {
-        return response.status(422).json({
-            key: "inexistentUser",
-            msg: "Usuário inexistente",
-        });
-    }
-    await banco.collection("usuarios").updateOne(
-        {
-            token,
-            created_at: { $gt: now.subtract({ hour: 1 }).toDate() },
-            used: false,
-        },
-        { $set: { used: true } }
-    );
-
-    await banco
-        .collection("usuarios")
-        .updateOne({ $set: { nome: existentUser.nome } }, { $set: { email: existentUser.email } });
-
-
-    return response.status(422).json({
-        key: "sucessfulyUpdateInfos",
-        msg: "Sucesso ao editar informações",
+  if (!banco) {
+    return response.status(500).json({
+      key: "internalServerError",
+      msg: "Erro interno do servidor",
     });
+  }
+  const now = moment();
+
+  const tokenValidated = await validateToken(token);
+  if (!tokenValidated) {
+    return response.status(301).redirect("http://localhost:3000/login");
+  }
+
+  await banco
+    .collection("usuarios")
+    .updateOne({ id: token.id }, { $set: { email, nome } });
+
+  return response.status(422).json({
+    key: "sucessfulyUpdateInfos",
+    msg: "Sucesso ao editar informações",
+  });
 }
